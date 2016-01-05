3f835158325bb6c472a3d3fe97addae5da050bf3
Stratos Pavlakis <pavlakis.stratos@gmail.com>
2016-01-05 17:48:47 +0200

    Setup the state of buttons on view load

--- MemeEditor/EditorViewController.swift
+++ MemeEditor/EditorViewController.swift
@@ -10,26 +10,24 @@ import UIKit
 
 class EditorViewController: UIViewController {
 
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        imageView.contentMode = .ScaleAspectFit
+        shareButton.enabled = false
+        cameraButton.enabled = isCameraAvailable()
     }
-    */
-
+    
+    
+    private func isCameraAvailable() -> Bool {
+       return UIImagePickerController.isSourceTypeAvailable(.Camera)
+    }
+    
 }
